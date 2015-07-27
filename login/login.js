6f7c923fbfadadb3e6e637d805b2757f611a4c67
SirPython <SirPython@users.noreply.github.com>
2015-07-27 15:13:47 -0400

    port not needed

--- login/login.js
+++ login/login.js
@@ -2,12 +2,10 @@
     var login_information = document.login_information;
     
     var server = login_information.server;
-    var port = login_information.port;
     var username = login_information.username;
     
     login_information.submit.onclick = function() {
         console.log("Server: " + server.value);
-        console.log("Port: " + port.value);
         console.log("Username: " + username.value);
     }
     
