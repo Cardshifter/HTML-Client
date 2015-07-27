8e71d8cf48be33e87f5e11247130344e612d33b6
SirPython <SirPython@users.noreply.github.com>
2015-07-26 22:05:46 -0400

    having trouble with global, will set inheritance using for loop tomorrow

--- server_interface/server_interface.js
+++ server_interface/server_interface.js
@@ -11,6 +11,32 @@
                 this.gamdId = gameId;
                 this.id = id;
                 this.action = action;
+            },
+            ServerQueryMessage: function(request, message) {
+                this.request = request;
+                this.message = message;
+                
+                this.toString = function() {
+                    return "ServerQueryMessage: Request" + this.request + " message: " + this.message;
+                }
+            },
+            StartGameRequest: function(opponent, gameType) {
+                this.opponent = opponent;
+                this.gameType = gameType;
+            },
+            TransformerMessage: function(type) {
+                this.type = type;
+            },
+            UseAbilityMessage: function(gameId, id, action, targets) {
+                this.gameId = gameId;
+                this.id = id;
+                this.action = action;
+                this.targets = targets;
+                
+                this.toString = function() {
+                    return "UseAbilityMessage [id=" + this.id + ", action=" + this.action
+				+ ", gameId=" + this.gameId + ", targets=" + this.targets.toString() + "]";
+                }
             }
         },
         init: function() {
@@ -18,6 +44,10 @@
             
             types.LoginMessage.prototype = new Message("login");
             types.RequestTargetsMessage.prototype = new Message("requestTargets");
+            types.ServerQueryMessage.prototype = new Message("query");
+            types.StartGameRequest.prototype = new Message("startgame");
+            types.TransformerMessage.prototype = new Message("serial");
+            types.UseAbilityMessage.prototype = new Message("use");
         }
     }
-})(Function("return this"));
+})(Function("return this")());
