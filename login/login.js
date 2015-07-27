f260556c5cf4f25f7e52e0fb299823752a26bd7a
SirPython <SirPython@users.noreply.github.com>
2015-07-27 16:59:06 -0400

    added a secure server option

--- login/login.js
+++ login/login.js
@@ -3,10 +3,12 @@
     
     var server = login_information.server;
     var username = login_information.username;
+    var isSecure = login_information.secure;
     
     login_information.submit.onclick = function() {
         console.log("Server: " + server.value);
         console.log("Username: " + username.value);
+        console.log("Is secure: " + isSecure.value);
     }
     
 })(Function("return this")());
