b6b7c4db667399d1b0fca6b18134370bd5b8dbed
SirPython <SirPython@users.noreply.github.com>
2015-07-27 20:36:31 -0400

    new select feature

--- login/login.js
+++ login/login.js
@@ -2,13 +2,27 @@
     var login_information = document.login_information;
     
     var server = login_information.server;
+    var serverOther = login_information.server_other;
+    var serverOtherContainer = document.getElementById("server_other");
     var username = login_information.username;
     var isSecure = login_information.secure;
+    var submit = login_information.submit;
+    var isOther = false;
     
-    login_information.submit.onclick = function() {
-        console.log("Server: " + server.value);
-        console.log("Username: " + username.value);
-        console.log("Is secure: " + isSecure.value);
+    submit.onclick = function() {
+        var finalServer = (isOther ? serverOther.value : server.value);
+        console.log(finalServer);
+    }
+    
+    server.onclick = function() {
+        console.log(this.value);
+        if(this.value === "other") {
+            serverOtherContainer.style.display = "block";
+            isOther = true;
+        } else {
+            serverOtherContainer.style.display = "none";
+            isOther = false;
+        }
     }
     
 })(Function("return this")());
