89e3c4e9303baf2d0c32480c05097dbd1312a66a
Francis Veilleux-Gaboury <Phrancis@users.noreply.github.com>
2015-08-04 14:33:43 -0400

    Update login.js
    Added temporary checks to print input values to console and append into document

--- login/login.js
+++ login/login.js
@@ -8,20 +8,43 @@
     var serverOtherContainer = document.getElementById("server_other_input");
     var username = loginInformation.username;
     var isSecure = loginInformation.secure;
+    var testMessage = loginInformation.test_message;
     var submit = loginInformation.submit;
     var isOther = false;
     
 
     submit.onclick = function() {
-        /* Verify if user selected one of the provided servers, or entered another server of their choosing */
+        // Verify if user selected one of the provided servers, or entered another server of their choosing
         var finalServer = (isOther ? serverOther.value : server.value);
-        console.log(finalServer);
+
+        // For testing print values to console
+        console.log("Final server: " + finalServer);
+        console.log("Username: " + username.value);
+        console.log("Is Secure: " + isSecure.value);
+        console.log("Test message: " + testMessage.value);
+
+        // Append input values to document prior to printing test results
+
+        websocketOutput = document.getElementById("websocket_output");
+
+        var wsHeader = document.createElement("h3");
+        wsHeader.innerHTML = "Testing WebSocket connection...";
+        websocketOutput.appendChild(wsHeader);
+        
+        var printTestMessage = (testMessage.value ? testMessage.value : "No test message");     
+        
+        var wsInput = document.createElement("p");
+        wsInput.innerHTML =
+            "Server: " + finalServer + "<br/>" +
+            "Username: " + username.value + "<br/>" +
+            "Message: " + printTestMessage;
+        websocketOutput.appendChild(wsInput);
     }
     
 
     server.onclick = function() {
-        console.log(this.value);
-        /* Display a text input field if user select "other" as server so that server can be entered manually */
+        console.log("Select server:" + this.value);
+        // Display a text input field if user select "other" as server so that server can be entered manually
         if(this.value === "other") {
             serverOtherContainer.style.display = "block";
             isOther = true;
