6bfc1904d629e1e41e53388ff2f47b64275c58e5
SirPython <anoky96@gmail.com>
2015-08-05 16:48:55 -0400

    added routing. currently, only page that opens is the login page, and that is the default page

--- cardshifter.js
+++ cardshifter.js
@@ -1,2 +1,21 @@
-var CardshifterApp = angular.module("CardshifterApp", []);
+var CardshifterApp = angular.module("CardshifterApp", ["ngRoute"]);
 
+CardshifterApp.config(function($routeProvider) {
+	$routeProvider
+		.when("/", { // default page is Login
+			controller: "LoginController",
+			// file name might change
+			templateUrl: "login/login_angular.html"
+		})
+		/*.when("/login", {
+			controller: "LoginController",
+			// file name might change
+			templateUrl: "login/login_angular.html"
+		})*/
+		/*.when("/lobby", {
+
+		})
+		.when("/game", {
+
+		})*/
+});
