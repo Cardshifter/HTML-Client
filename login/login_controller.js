bfe1749bf2cd4a4f66f590421a5d6957480062b4
SirPython <anoky96@gmail.com>
2015-08-06 17:22:48 -0400

    removed console.logs.. excuse the content, horrible typing mistakes were made.

--- login/login_controller.js
+++ login/login_controller.js
@@ -5,16 +5,13 @@ CardshifterApp.controller("LoginController", function($scope, $location, $rootSc
 
 		CardshifterServerAPI.init(finalServer, $scope.is_secure, function() {
 			var login = CardshifterServerAPI.messageTypes.LoginMessage($scope.username);
-			console.log("suck chess");
 
 			try {
-				console.log("Sending login and changing path");
 				CardshifterServerAPI.sendMessage(login);
 
 				$rootScope.$apply(function() {
 					$location.path("/lobby");
 				});
-				console.log("shud chang");
 			} catch(e) {
 				// notify the user that there was an issue logging in (loginmessage issue)
 				console.log("LoginMessage error(error 2)");
