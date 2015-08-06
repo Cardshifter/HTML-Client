5c6e86b6c8febc6b809ee7d003a013ca5ec75dbe
Francis Veilleux-Gaboury <djphrancis@gmail.com>
2015-08-05 21:27:03 -0400

    Commented out login send message, added TODO

--- login/login_controller.js
+++ login/login_controller.js
@@ -4,10 +4,13 @@ CardshifterApp.controller("LoginController", function($scope) {
 
 		CardshifterServerAPI.init(finalServer, $scope.is_secure);
 		var login =  new CardshifterServerAPI.messageTypes.LoginMessage($scope.username);
-		try {
+
+		// TODO: Need to find a way to make this work;
+		// As written it tries to sendMessage the instant that the socket is created, in which case the socket is not ready
+/*		try {
 			CardshifterServerAPI.sendMessage(login);
 		} catch (e) {
 			console.log(e);
-		}
+		}*/
 	}
 });
