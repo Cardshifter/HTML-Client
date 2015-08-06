afd60bfd311fc0777cdfe3bb68a462c1f6d45807
SirPython <anoky96@gmail.com>
2015-08-05 22:48:38 -0400

    now implements new API feature: onReady, switches to lobby screen.
    Now, when initializing the API after the user has entered their login credentials, the code uses the onReady parameter of the .init function. This prevents the code from attempting to do any interaction with the socket before it is ready. However, the code does not have a spot to tell the user if they entered something incorrectly.
    
    Also, upon sending the login request, the code will now load the lobby screen.
    
    Idea: add an onError argument to the API init function. It should be clear what this is for.

--- login/login_controller.js
+++ login/login_controller.js
@@ -1,16 +1,17 @@
-CardshifterApp.controller("LoginController", function($scope) {
+CardshifterApp.controller("LoginController", function($scope, $location) {
 	$scope.login = function() {
 		var finalServer = ($scope.server === "other" ? $scope.other_server : $scope.server);
 
-		CardshifterServerAPI.init(finalServer, $scope.is_secure);
-		var login =  new CardshifterServerAPI.messageTypes.LoginMessage($scope.username);
+		CardshifterServerAPI.init(finalServer, $scope.is_secure, function() {
+			var login = CardshifterServerAPI.messageTypes.LoginMessage($scope.username);
 
-		// TODO: Need to find a way to make this work;
-		// As written it tries to sendMessage the instant that the socket is created, in which case the socket is not ready
-/*		try {
-			CardshifterServerAPI.sendMessage(login);
-		} catch (e) {
-			console.log(e);
-		}*/
+			try {
+				console.log("Sending login and changing path");
+				CardshifterServerAPI.sendMessage(login);
+				$location.path("/lobby");
+			} catch(e) {
+				// notify the user that there was an issue logging in
+			}
+		});
 	}
 });
