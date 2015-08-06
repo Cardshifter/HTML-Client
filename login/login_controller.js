024b896c236cb38d910ec72d4515be6dbe454280
SirPython <anoky96@gmail.com>
2015-08-06 17:19:19 -0400

    fixed double click bug, disables button after click
    Before, there was  bug where, when attempting to log in, the user would be forced to rapidly double click the "Log In" button in order for the screen to change successfully. Now, using something that I read on StackOverflow, the button only needs to be clicked a single time. This utilizes $rootScope and $rootScope's $apply method.
    
    Now, on the login page, after the user has entered their credentials and they have hit the "Log In" button, this button becomes instantly disabled so that multiple requests aren't sent to the server.

--- login/login_controller.js
+++ login/login_controller.js
@@ -1,14 +1,20 @@
-CardshifterApp.controller("LoginController", function($scope, $location) {
+CardshifterApp.controller("LoginController", function($scope, $location, $rootScope) {
 	$scope.login = function() {
+		$scope.loggedIn = true;
 		var finalServer = ($scope.server === "other" ? $scope.other_server : $scope.server);
 
 		CardshifterServerAPI.init(finalServer, $scope.is_secure, function() {
 			var login = CardshifterServerAPI.messageTypes.LoginMessage($scope.username);
+			console.log("suck chess");
 
 			try {
 				console.log("Sending login and changing path");
 				CardshifterServerAPI.sendMessage(login);
-				$location.path("/lobby");
+
+				$rootScope.$apply(function() {
+					$location.path("/lobby");
+				});
+				console.log("shud chang");
 			} catch(e) {
 				// notify the user that there was an issue logging in (loginmessage issue)
 				console.log("LoginMessage error(error 2)");
