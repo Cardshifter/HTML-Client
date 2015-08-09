f20d135903dcb4da6aa51e5885920dd01b1dcfd3
SirPython <anoky96@gmail.com>
2015-08-08 21:23:41 -0400

    sets up card maximum values, create showDetails function, created getTotalSelected function, created increment and decrement functions
    When the first message is received from the server describing all the cards, a new property is set to each card called .max. This will either hold the card's personal maximum hold limit, or it will hold the default maximum hold limit.
    
    The showDetails function will display all the information about a card at the top of the document. Right now, it is extremely ugly.
    
    The getTotalSelected function will sum up all the values in $scope.cardChoice (which is all the cards and how many times the user has selected them) and return the total amount
    
    The increment and decrement functions correspond to the + and - buttons of each card. The increment will not go over the maximum allowed cards and the maximum allowed cards for that specific card, and the decrement function will not let the card amount go below 0 for that specific card.

--- deck_builder/deck_builder_controller.js
+++ deck_builder/deck_builder_controller.js
@@ -1,3 +1,48 @@
 CardshifterApp.controller("DeckbuilderController", function($scope) {
+    $scope.cards = [];
+    $scope.maxCards;
+    $scope.cardChoices = {};
+    $scope.cardInformation;
 
+    CardshifterServerAPI.setMessageListener(function(cardInformation) {
+        var deck = cardInformation.configs.Deck;
+
+        for(card in deck.cardData) {
+            deck.cardData[card].max = (deck.max[card] ? deck.max[card] : deck.maxPerCard); // bad to add property?
+            $scope.cardChoices[deck.cardData[card].properties.name] = 0;
+        }
+
+        $scope.cards = deck.cardData;
+        $scope.maxCards = deck.maxSize;
+
+        $scope.$apply();
+    }, ["playerconfig"]);
+
+    $scope.showDetails = function(card) {
+        $scope.cardInformation = "";
+        for(var property in card.properties) {
+            $scope.cardInformation += (property + ": " + card.properties[property] + "\n");
+        }
+    }
+
+    $scope.getTotalSelected = function() {
+        var total = 0;
+        for(var card in $scope.cardChoices) {
+            total += $scope.cardChoices[card];
+        }
+        return total;
+    }
+
+    $scope.increment = function(card) {
+        if($scope.getTotalSelected() !== $scope.maxCards) {
+            if($scope.cardChoices[card.properties.name] !== card.max) {
+                $scope.cardChoices[card.properties.name]++;
+            }
+        }
+    }
+    $scope.decrement = function(card) {
+        if($scope.cardChoices[card.properties.name] !== 0) {
+            $scope.cardChoices[card.properties.name]--;
+        }
+    }
 });
