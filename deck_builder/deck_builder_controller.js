6ec47dd48a855c9f887e904abaf7d70184c1ea04
SirPython <anoky96@gmail.com>
2015-08-09 15:32:08 -0400

    added deck deletion, repurposed deckExists, change storing method
    There is now a function $scope.deleteDeck that takes the name of a deck, searches for the deck by name in the current mod category, and removes it.
    
    The function deckExists used to return true if the deck exists, and false if it did not. However, now it will return the index at which the deck was found, or it will return false. This allows for the $scope.deleteDeck function to not have to create a loop just to get the index of the deck. And, since numbers are treated as a true-y value in JavaScript, this function will behave the same way in $scope.saveDeck
    
    Before, decks would just be stored in LocalStorage with a name and a string. However, I soon realized a problem: you would see decks from ALL other mods in the deck creator, which can only lead to trouble. Now, the storage is formatted like this: {decks: {"mod1": [...decks...], "mod2": [...decks...]}}. That way, decks are saved with whichever mod they correspond to.

--- deck_builder/deck_builder_controller.js
+++ deck_builder/deck_builder_controller.js
@@ -8,25 +8,33 @@ CardshifterApp.controller("DeckbuilderController", function($scope) {
     $scope.cardInfo = {};
     $scope.currentDeckName = "untitled";
     $scope.savedDecks = [];
+    $scope.doneLoading = false;
 
     if(!localStorage.getItem(DECK_STORAGE)) {
-        localStorage.setItem(DECK_STORAGE, "{\"decks\": []}");
+        var json = "{\"decks\": {";
+        for(var i = 0, length = availableGameMods.length; i < length; i++) {
+            json += "\"" + availableGameMods[i] + "\":[],";
+        }
+        json = json.slice(0, -1); // remove last comma
+        json += "}}";
+        localStorage.setItem(DECK_STORAGE, json);
     }
 
     CardshifterServerAPI.setMessageListener(function(cardInformation) {
         var deck = cardInformation.configs.Deck;
-        console.log(deck);
-
-        window.SIRPYTHONTESTINGINCORPORATED = cardInformation;
 
         for(var card in deck.cardData) {
-            deck.cardData[card].max = deck.max[card] || deck.maxPerCard;
-            $scope.currentDeck[deck.cardData[card].id] = 0;
+            if(deck.cardData.hasOwnProperty(card)) {
+                deck.cardData[card].max = deck.max[card] || deck.maxPerCard;
+                $scope.currentDeck[deck.cardData[card].id] = 0;
+            }
         }
 
         $scope.cards = deck.cardData;
         $scope.maxCards = deck.maxSize;
         $scope.minCards = deck.minSize;
+        updateSavedDecks();
+        $scope.doneLoading = true;
 
         $scope.$apply();
     }, ["playerconfig"]);
@@ -35,21 +43,24 @@ CardshifterApp.controller("DeckbuilderController", function($scope) {
         if($scope.currentDeck[card.id] !== 0) {
             $scope.currentDeck[card.id]--;
         }
-    }
+    };
     $scope.increment = function(card) {
         if($scope.getTotalSelected() !== $scope.maxCards &&
            $scope.currentDeck[card.id] !== card.max) {
             $scope.currentDeck[card.id]++;
         }
-    }
+    };
 
     $scope.getTotalSelected = function() {
-        var total = 0
+        var total = 0;
         for(var card in $scope.currentDeck) {
-            total += $scope.currentDeck[card];
+            if($scope.currentDeck.hasOwnProperty(card)) {
+                total += $scope.currentDeck[card];
+            }
+
         }
         return total;
-    }
+    };
 
     $scope.showDetails = function(card) {
         var props = card.properties;
@@ -61,12 +72,12 @@ CardshifterApp.controller("DeckbuilderController", function($scope) {
             attack: props.ATTACK,
             sickness: props.SICKNESS
         };
-    }
+    };
 
     $scope.saveDeck = function() {
         if($scope.getTotalSelected() === $scope.minCards) {
             if($scope.deckName) {
-                if(!deckExists($scope.deckName)) {
+                if(!getDeckIndex($scope.deckName)) { // if deck exists
                     var savedDecks = JSON.parse(localStorage.getItem(DECK_STORAGE));
 
                     var newDeck = {
@@ -74,7 +85,7 @@ CardshifterApp.controller("DeckbuilderController", function($scope) {
                         cards: $scope.currentDeck
                     };
 
-                    savedDecks.decks.push(newDeck);
+                    savedDecks.decks[currentUser.game.mod].push(newDeck);
                     localStorage.setItem(DECK_STORAGE, JSON.stringify(savedDecks));
                     updateSavedDecks();
 
@@ -88,19 +99,26 @@ CardshifterApp.controller("DeckbuilderController", function($scope) {
         } else {
             console.log("not enough cards");
         }
-    }
+    };
     $scope.switchDeck = function(deck) {
         $scope.currentDeckName = deck.name;
         $scope.currentDeck = deck.cards;
-    }
+    };
+    $scope.deleteDeck = function(deckName) {
+        var savedDecks = JSON.parse(localStorage.getItem(DECK_STORAGE));
+        savedDecks.decks[currentUser.game.mod].splice(getDeckIndex(deckName), 1);
+        localStorage.setItem(DECK_STORAGE, JSON.stringify(savedDecks));
+
+        updateSavedDecks();
+    };
 
     function updateSavedDecks() {
-        $scope.savedDecks = JSON.parse(localStorage.getItem(DECK_STORAGE)).decks;
+        $scope.savedDecks = JSON.parse(localStorage.getItem(DECK_STORAGE)).decks[currentUser.game.mod];
     }
-    function deckExists(name) {
+    function getDeckIndex(deckName) {
         for(var i = 0, length = $scope.savedDecks.length; i < length; i++) {
-            if($scope.savedDecks[i].name === name) {
-                return true;
+            if($scope.savedDecks[i].name === deckName) {
+                return i;
             }
         }
         return false;
