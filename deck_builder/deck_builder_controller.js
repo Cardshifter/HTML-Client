ca9c218430bcdcf758b2da4822d22426ff487cb5
SirPython <anoky96@gmail.com>
2015-08-09 12:24:36 -0400

    cards are now handled by ID rather than name, updated showDetails function to now create an object cardInfo with all the NECESSARY information

--- deck_builder/deck_builder_controller.js
+++ deck_builder/deck_builder_controller.js
@@ -1,48 +1,56 @@
 CardshifterApp.controller("DeckbuilderController", function($scope) {
     $scope.cards = [];
-    $scope.maxCards;
-    $scope.cardChoices = {};
-    $scope.cardInformation;
+    $scope.maxCards = 0;
+    $scope.minCards = 0;
+    $scope.currentDeck = {};
+    $scope.cardInfo = {};
 
     CardshifterServerAPI.setMessageListener(function(cardInformation) {
         var deck = cardInformation.configs.Deck;
 
-        for(card in deck.cardData) {
-            deck.cardData[card].max = (deck.max[card] ? deck.max[card] : deck.maxPerCard); // bad to add property?
-            $scope.cardChoices[deck.cardData[card].properties.name] = 0;
+        window.SIRPYTHONTESTINGINCORPORATED = cardInformation;
+
+        for(var card in deck.cardData) {
+            deck.cardData[card].max = deck.max[card] || deck.maxPerCard;
+            $scope.currentDeck[deck.cardData[card].id] = 0;
         }
 
         $scope.cards = deck.cardData;
         $scope.maxCards = deck.maxSize;
+        $scope.minCards = deck.minCards;
 
         $scope.$apply();
     }, ["playerconfig"]);
 
-    $scope.showDetails = function(card) {
-        $scope.cardInformation = "";
-        for(var property in card.properties) {
-            $scope.cardInformation += (property + ": " + card.properties[property] + "\n");
+    $scope.decrement = function(card) {
+        if($scope.currentDeck[card.id] !== 0) {
+            $scope.currentDeck[card.id]--;
+        }
+    }
+    $scope.increment = function(card) {
+        if($scope.getTotalSelected() !== $scope.maxCards &&
+           $scope.currentDeck[card.id] !== card.max) {
+            $scope.currentDeck[card.id]++;
         }
     }
 
     $scope.getTotalSelected = function() {
-        var total = 0;
-        for(var card in $scope.cardChoices) {
-            total += $scope.cardChoices[card];
+        var total = 0
+        for(var card in $scope.currentDeck) {
+            total += $scope.currentDeck[card];
         }
         return total;
     }
 
-    $scope.increment = function(card) {
-        if($scope.getTotalSelected() !== $scope.maxCards) {
-            if($scope.cardChoices[card.properties.name] !== card.max) {
-                $scope.cardChoices[card.properties.name]++;
-            }
-        }
-    }
-    $scope.decrement = function(card) {
-        if($scope.cardChoices[card.properties.name] !== 0) {
-            $scope.cardChoices[card.properties.name]--;
-        }
+    $scope.showDetails = function(card) {
+        var props = card.properties;
+        $scope.cardInfo = {
+            name: props.name,
+            flavor: props.flavor,
+            type: props.creatureType,
+            health: props.HEALTH,
+            attack: props.ATTACK,
+            sickness: props.SICKNESS
+        };
     }
 });
