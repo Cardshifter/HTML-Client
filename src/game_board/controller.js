4617ef585b50c85c7f69d57fab870c6d7ad83386
SirPython <anoky96@gmail.com>
2015-08-10 14:46:01 -0400

    updated all code from merge to be newest, added game_board
    Now, like before with zomis.net and dwarftowers.com, the client will not connect to the local host server (at least for me). This is being put of for other people to look at and hopefully help fix.

---
+++ src/game_board/controller.js
@@ -0,0 +1,8 @@
+'use strict';
+
+// @ngInject
+function GameboardController(CardshifterServerAPI, $scope, $timeout, $rootScope, $location) {
+    console.log("loaded");
+}
+
+module.exports = GameboardController;
