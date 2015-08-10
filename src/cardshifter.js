185f96ed85d760ff108ce16addac23c449a60f1a
Dan <dan@lambdaexpression.io>
2015-08-10 12:33:01 +0100

    alter cardshifter to require angular and ngRoute from webpack
    angular and ngRoute are split into their own chunk with webpack. It may be
    worth instead having them loaded from a CDN.

--- src/cardshifter.js
+++ src/cardshifter.js
@@ -1,5 +1,8 @@
-var CardshifterApp = angular.module("CardshifterApp", ["ngRoute"]);
+'use strict';
+var angular = require('angular');
+var ngRoute = require('angular-route');
 
+var CardshifterApp = angular.module("CardshifterApp", [ngRoute]);
 CardshifterApp.config(function($routeProvider) {
     $routeProvider
         .when("/", { // default page is Login
