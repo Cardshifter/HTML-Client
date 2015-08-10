7b2ee92c8326f680dc53811c9f3c0296ea87181d
Dan <dan@lambdaexpression.io>
2015-08-10 16:03:47 +0100

    s/cardshifter.login/cardshifter.deckBuilder

--- src/deck_builder/module.js
+++ src/deck_builder/module.js
@@ -5,7 +5,7 @@ var DeckBuilderController = require('./controller');
 var serverInterface = require('../server_interface/module');
 var template = require('./template.html');
 
-module.exports = angular.module('cardshifter.login', [ngRoute, serverInterface.name])
+module.exports = angular.module('cardshifter.deckBuilder', [ngRoute, serverInterface.name])
   .config(function($routeProvider) {
     $routeProvider.when("/deck_builder", {
         controller: DeckBuilderController,
