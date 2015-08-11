92ee000de373fe601afbb613b1a954374f7e4f61
SirPython <SirPython@users.noreply.github.com>
2015-08-11 17:51:39 -0400

    setup debugging tools
    This commit is to store the current code in the case that I break something when trying to use bisect

--- src/game_board/controller.js
+++ src/game_board/controller.js
@@ -8,11 +8,15 @@ function GameboardController(CardshifterServerAPI, $scope, $timeout, $rootScope,
         "cardInfo": debug
     };
 
+    var i = 0;
     CardshifterServerAPI.setMessageListener(function(message) {
-        commandMap[message.command](message);
-    }, ["zoneMessage", "cardInfo"]);
+        //commandMap[message.command](message);
+        debug(message);
+    });
 
     function debug(message) {
+        i++;
+        console.log(i);
         console.log("Received a [" + message.command + "] message:");
         console.log(message);
         console.log("----------");
