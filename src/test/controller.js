1cf559f7fdbe94f423fd78c72d9b5c7e34973d1e
Simon Forsberg <zomis2k@hotmail.com>
2015-08-24 16:32:34 +0200

    added more properties to test card

--- src/test/controller.js
+++ src/test/controller.js
@@ -5,12 +5,16 @@ function TestController(CardshifterServerAPI, $scope, $timeout, $rootScope, $loc
     $scope.zoneInfo = {
         entities: {
             14: {
+                id: 14,
                 properties: {
                     name: 'Hello',
                     imagePath: 'mythos/default.png',
-                    HEALTH: 4,
+                    HEALTH: 42,
                     ATTACK: 1,
                     MANA_COST: 45,
+                    flavor: 'This is a test',
+                    creatureType: 'Creature Type',
+                    effect: 'This is a description that can be very long that we somehow have to fit into the card. It can also contain\nmultiple lines.'
                 }
             },
         }
