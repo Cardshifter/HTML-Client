b0a5c1cff1c024186b934a66366a208b351ad9f4
Jacob Wahlgren <jacob.wahlgren@gmail.com>
2015-09-13 12:34:34 +0200

    WIP: Post to Duga on deploy

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,3 +1,5 @@
+var http = require('http');
+var url = require('url');
 var FtpDeploy = require('ftp-deploy');
 var ftpDeploy = new FtpDeploy();
 
@@ -10,6 +12,49 @@ var ftpConfig = {
     remoteRoot: "/"
 };
 
+var chatBotRequest = {
+    apiKey: process.env.DEPLOY_DUGA_KEY,
+    roomId: 16134,
+    text: "New web client version uploaded."
+};
+
+var chatBotConfig = {
+    hostname: "stats.zomis.net",
+    path: "/GithubHookSEChatService/bot/jsonPost",
+    method: "POST",
+    headers: {
+        "Content-Type": "application/json"
+    }
+}
+
+function postToChat(config, botRequest) {
+    var json = JSON.stringify(botRequest, ["url", "apiKey", "roomId", "text"]);
+    config.headers["Content-Length"] = json.length;
+
+    console.log("Posting message to " + url.format(config) + "...");
+    var req = http.request(config);
+
+    req.on("connect", function(response, socket, head) {
+        console.log("Chat bot response status: " + response.statusMessage);
+
+        socket.on("data", function(chunk) {
+            console.log("Chat bot: " + chunk.toString());
+        });
+
+        socket.on("error", function(err) {
+            console.log("Socket error: " + err);
+        });
+
+        socket.on("close", function(had_error) {
+            if (!had_error) {
+                console.log("Post successful");
+            }
+        });
+    });
+
+    req.end(json);
+}
+
 console.log("Deploying to ftp://" + ftpConfig.host + ":" + ftpConfig.port + "...");
 
 ftpDeploy.deploy(ftpConfig, function(err) {
@@ -18,7 +63,7 @@ ftpDeploy.deploy(ftpConfig, function(err) {
         process.exit(1);
     } else {
         console.log("Deployment successful.");
-        process.exit(0);
+        postToChat(chatBotConfig, chatBotRequest);
     }
 });
 
