0b3b8a24821b75edf64a4326540a866302709252
Jacob Wahlgren <jacob.wahlgren@gmail.com>
2015-09-13 18:08:17 +0200

    Copy all files to temp dir before deploying
    The ftp-deploy library doesn't work very well when uploading from
    multiple directories.

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,14 +1,19 @@
+var copy = require('recursive-copy');
 var FtpDeploy = require('ftp-deploy');
 var ftpDeploy = new FtpDeploy();
+var path = require('path');
+var temp = require('temp').track();
 var request = require('request');
 
-var ftpConfig = {
-    username: process.env.DEPLOY_FTP_USERNAME,
-    password: process.env.DEPLOY_FTP_PASSWORD,
-    host: "localhost",
-    port: 2121,
-    localRoot: __dirname + "/../dist",
-    remoteRoot: "/"
+function ftpConfig(local, remote) {
+    return {
+        username: process.env.DEPLOY_FTP_USERNAME,
+        password: process.env.DEPLOY_FTP_PASSWORD,
+        host: "localhost",
+        port: 2121,
+        localRoot: local,
+        remoteRoot: remote
+    };
 };
 
 var chatBotRequest = {
@@ -37,7 +42,7 @@ function postToChat(config, botRequest) {
     var req = request(config, function(error, response, body) {
         console.log("Response status: " + response.statusMessage);
         if (error) {
-            console.log("Request error: " + error);
+            throw error;
         }
         if (body) {
             console.log("Response body:\n" + body);
@@ -45,15 +50,47 @@ function postToChat(config, botRequest) {
     });
 }
 
-console.log("Deploying to ftp://" + ftpConfig.host + ":" + ftpConfig.port + "...");
+function setupFiles(callback) {
+    // ftp-deploy doesn't handle uploading from multiple directories well
+    temp.mkdir("cardshifter-deploy", function (err, tempDir) {
+        if (err) {
+            throw err;
+        }
+        copy(path.join(__dirname, "..", "www"), tempDir, function(err, res) {
+            if (err) {
+                throw err;
+            }
+            copy(path.join(__dirname, "..", "dist"), path.join(tempDir, "assets"), function(err, res) {
+                if (err) {
+                    throw err;
+                }
+                if (callback) {
+                    callback(tempDir);
+                }
+            });
+            
+        });
+    });
+}
 
-ftpDeploy.deploy(ftpConfig, function(err) {
-    if (err) {
-        console.log(err);
-        process.exit(1);
-    } else {
-        console.log("Deployment successful.");
-        postToChat(chatBotConfig, chatBotRequest);
-    }
+function deployFtp(config, callback) {
+    ftpDeploy.deploy(config, function(err) {
+        if (err) {
+            throw err;
+        } else {
+            if (callback) {
+                callback();
+            }
+        }
+    });
+}
+
+setupFiles(function(dir) {
+    var config = ftpConfig(dir, "/");
+    console.log("Deploying to ftp://" + config.host + ":" + config.port + "...");
+    deployFtp(config, function() {
+            console.log("FTP deployment successful");
+            postToChat(chatBotConfig, chatBotRequest);
+    });
 });
 
