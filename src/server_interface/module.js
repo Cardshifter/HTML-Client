32082f4827e473e72902f318c4a645ea3a9c8560
SirPython <SirPython@users.noreply.github.com>
2015-10-16 20:24:52 -0400

    now displays api information in the browser console

--- src/server_interface/module.js
+++ src/server_interface/module.js
@@ -1,9 +1,11 @@
 'use strict';
 var angular = require('angular');
 var serverInterface = require('./server_interface');
-var debug = require("debug")("api");
+var debug = require("debug");
+
+debug.enable("cardshifter:*");
 
 // TODO: This is not great but it will do for a decent middleground
 module.exports = angular.module('cardshifter.api', [])
     .factory('CardshifterServerAPI', serverInterface)
-    .constant("debug", debug);
+    .constant("debug", debug("cardshifter:api"));
