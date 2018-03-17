90e325ef30a97dbb4e7733e70859c3ac7632d60d
Phrancis <Phrancis@users.noreply.github.com>
2018-03-17 16:00:32 -0400

    fix a name error in global object, and replace hard coded strings with variable names

--- www/global.js
+++ www/global.js
@@ -2,7 +2,7 @@
  * This IIFE is for global variables / constants to be used throughout the site.
  */
 
-const globals = {
+const global = {
     /*
      * Setting `debugMode` to `true` will print console messages in the browser
      * to help in debugging and keeping track of what is happening on the page.
