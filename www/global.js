a5ab782577431f94efaedef97a49b4f51b085a9d
Phrancis <Phrancis@users.noreply.github.com>
2018-03-17 16:05:50 -0400

    Changed `global` object to just variables, seems to make more sense right now

--- www/global.js
+++ www/global.js
@@ -1,12 +1,10 @@
 /*
- * This IIFE is for global variables / constants to be used throughout the site.
+ * This file is for global values to be used throughout the site.
  */
 
-const global = {
-    /*
-     * Setting `debugMode` to `true` will print console messages in the browser
-     * to help in debugging and keeping track of what is happening on the page.
-     * This should be set to `false` on the public client.
-     */
-    debug : true
-};
+/*
+ * Setting to `true` will log messages in the browser console
+ * to help in debugging and keeping track of what is happening on the page.
+ * This should be set to `false` on the public client.
+ */
+const DEBUG = true;
