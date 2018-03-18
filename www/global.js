d22fd76b612ec368cf6c3373c960edec45f02ce0
Phrancis <Phrancis@users.noreply.github.com>
2018-03-17 22:56:22 -0400

    execution needs Wrong order, Promises

--- www/global.js
+++ www/global.js
@@ -9,3 +9,18 @@
  * This should be set to `false` on the public client.
  */
 const DEBUG = true;
+
+/*
+ * Port number used for WebSocket.
+ */
+const WS_PORT = 4243;
+
+/*
+ * List of game server names and WebSocket URIs.
+ */
+const GAME_SERVERS = {
+    "localhost" : `ws://127.0.0.1:${WS_PORT}`,
+    "dwarftowers.com" : `ws://dwarftowers.com:${WS_PORT}`,
+    "zomis.net" : `ws://stats.zomis.net:${WS_PORT}`,
+    "Other" : null
+};
---
+++ www/sections/login/login.js
@@ -0,0 +1,14 @@
+/* global GAME_SERVERS */
+console.log("FOO");
+const selectLoginServerList = document.getElementById("login_server_list");
+console.log(selectLoginServerList.outerHTML);
+
+for (let key in GAME_SERVERS) {
+    console.log("inside GAME_SERVERS loop");
+    if (GAME_SERVERS.hasOwnProperty(key)) {
+        const option = document.createElement("option");
+        option.text = key;
+        option.value = GAME_SERVERS[key];
+        selectLoginServerList.add(option);
+    }
+}
