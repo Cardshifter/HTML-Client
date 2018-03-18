3fb64d3a368e61adaf7af2fb0c095c89e8e3b8b4
Phrancis <Phrancis@users.noreply.github.com>
2018-03-18 04:51:41 -0400

    Populate server list at login.

--- www/sections/login/login.js
+++ www/sections/login/login.js
@@ -1,14 +1,13 @@
 /* global GAME_SERVERS */
-//console.log("FOO");
-//const selectLoginServerList = document.getElementById("login_server_list");
-//console.log(selectLoginServerList.outerHTML);
-//
-//for (let key in GAME_SERVERS) {
-//    console.log("inside GAME_SERVERS loop");
-//    if (GAME_SERVERS.hasOwnProperty(key)) {
-//        const option = document.createElement("option");
-//        option.text = key;
-//        option.value = GAME_SERVERS[key];
-//        selectLoginServerList.add(option);
-//    }
-//}
+
+const loginHandler = function() {
+    const selectLoginServerList = document.getElementById("login_server_list");
+    for (let key in GAME_SERVERS) {
+        if (GAME_SERVERS.hasOwnProperty(key)) {
+            const option = document.createElement("option");
+            option.text = key;
+            option.value = GAME_SERVERS[key];
+            selectLoginServerList.add(option);
+        }
+    }
+};
