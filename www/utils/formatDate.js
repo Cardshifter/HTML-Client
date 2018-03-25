458c80d39d0d76643f8faa7656004ce8c7102896
Phrancis <Phrancis@users.noreply.github.com>
2018-03-25 16:46:51 -0400

    Improved `formatDate` function

--- www/utils/formatDate.js
+++ www/utils/formatDate.js
@@ -2,45 +2,34 @@
 
 /**
  * Formats a Date object based on a format string, e.g., "yyyy/MM/dd hh:mm:ss"
- * Original source: 
- * https://dzone.com/articles/javascript-formatdate-function
- * Original source modified to fix a few bugs and modernize.
  * 
  * @param {Date} date - the Date to format
  * @param {String} formatString - the format string to use
  * @returns {String} - the formatted date
+ * @source https://dzone.com/articles/javascript-formatdate-function (but modified quite a bit)
  */
-const formatDate = function (date=new Date(), formatString=DEFAULT_DATE_FORMAT) {
-    if(date instanceof Date) {
-        const months = new Array("Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec");
-        const yyyy = date.getFullYear();
-        const yy = yyyy.toString().slice(-2);
-        const M = date.getMonth() + 1;
-        const MM = M < 10 ? `0${M}` : M;
-        const MMM = months[M - 1];
-        const d = date.getDate();
-        const dd = d < 10 ? `0${d}` : d;
-        const h = date.getHours();
-        const hh = h < 10 ? `0${h}` : h;
-        const m = date.getMinutes();
-        const mm = m < 10 ? `0${m}` : m;
-        const s = date.getSeconds();
-        const ss = s < 10 ? `0${s}` : s;
-        formatString = formatString.replace(/yyyy/, yyyy);
-        formatString = formatString.replace(/yy/, yy);
-        formatString = formatString.replace(/MMM/, MMM);
-        formatString = formatString.replace(/MM/, MM);
-        formatString = formatString.replace(/M/, M);
-        formatString = formatString.replace(/dd/, dd);
-        formatString = formatString.replace(/d/, d);
-        formatString = formatString.replace(/hh/, hh);
-        formatString = formatString.replace(/h/, h);
-        formatString = formatString.replace(/mm/, mm);
-        formatString = formatString.replace(/m/, m);
-        formatString = formatString.replace(/ss/, ss);
-        formatString = formatString.replace(/s/, s);
-        return formatString;
-    } else {
-        return "";
-    }
+const formatDate = function (date, formatString = DEFAULT_DATE_FORMAT) {
+  if (!(date instanceof Date)) {
+    return "";
+  }
+
+  const months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+  const format = {
+    yyyy: date.getFullYear(),
+    M: date.getMonth() + 1,
+    d: date.getDate(),
+    h: date.getHours(),
+    m: date.getMinutes(),
+    s: date.getSeconds()
+  };
+  format.yy = format.yyyy.toString().slice(-2);
+  format.MM = format.M < 10 ? `0${format.M}` : M;
+  format.MMM = months[date.getMonth()];
+  format.dd = format.d < 10 ? `0${format.d}` : format.d;
+  format.hh = format.h < 10 ? `0${format.h}` : format.h;
+  format.mm = format.m < 10 ? `0${format.m}` : format.m;
+  format.ss = format.s < 10 ? `0${format.s}` : format.s;
+
+  const regex = /yyyy|yy|MMM|MM|M|dd|d|hh|h|mm|m|ss|s/g;
+  return formatString.replace(regex, s => format[s] || s);
 };
