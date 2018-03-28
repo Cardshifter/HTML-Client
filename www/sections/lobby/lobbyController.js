768b13731e80948d4e0d248543de759c942a46ae
Phrancis <francis.v.gaboury@gmail.com>
2018-03-28 01:08:11 -0400

    Move logic to display username in top navbar to `topNavbarController.js`

--- www/sections/lobby/lobbyController.js
+++ www/sections/lobby/lobbyController.js
@@ -1,19 +1,6 @@
 "use strict";
 
 const lobbyController = function() {
-    const showUsernameIfLoggedIn = function() {
-        if (localStorage.getItem("loggedIn") === "true") {
-            const username = localStorage.getItem("username");
-            const separator = document.createElement("span");
-            separator.innerHTML = " | ";
-            const usernameDisplay = document.createElement("span");
-            usernameDisplay.className = "h4";
-            usernameDisplay.innerHTML = username;
-            document.getElementById("top_navbar_title").appendChild(separator);
-            document.getElementById("top_navbar_title").appendChild(usernameDisplay);
-        }
-    };
-    
     /**
      * IIFE to control the lobby.
      * @type undefined
--- www/sections/top_navbar/topNavbarController.js
+++ www/sections/top_navbar/topNavbarController.js
@@ -2,10 +2,29 @@
 
 const topNavbarController = function() {
     /**
-     * IIFE to run top navbar login.
+     * Appends the username to the top navbar along with a separator.
+     * @returns {undefined}
+     */
+    const showUsernameIfLoggedIn = function() {
+        if (localStorage.getItem("loggedIn") === "true") {
+            const username = localStorage.getItem("username");
+            const separator = document.createElement("span");
+            separator.innerHTML = " | ";
+            const usernameDisplay = document.createElement("span");
+            usernameDisplay.className = "h4";
+            usernameDisplay.innerHTML = username;
+            const topNavbarHeader = document.getElementById("top_navbar_header");
+            topNavbarHeader.appendChild(separator);
+            topNavbarHeader.appendChild(usernameDisplay);
+        }
+    };
+    
+    /**
+     * IIFE to run top navbar.
      * @type undefined
      */
     const runTopNavbarController = function() {
         logDebugMessage("runTopNavbarController called");
+        showUsernameIfLoggedIn();
     }();
 };
