7543b725d690f476b3b82b7e727eed06134a827f
Phrancis <Phrancis@users.noreply.github.com>
2018-04-01 03:47:09 -0400

    Skeleton for deck builder

---
+++ www/sections/deck_builder/deckBuilderController.js
@@ -0,0 +1,12 @@
+"use strict";
+
+const deckBuilderController = function() {
+
+    /**
+     * IIFE to control the deck builder.
+     * @type undefined
+     */
+    const runDeckBuilderController = function() {
+        logDebugMessage("deckBuilderController called");
+    }();
+};
