8f8656dacdaab11ed0877c2d0db12aa47c0ed3f2
Phrancis <Phrancis@users.noreply.github.com>
2018-04-29 00:35:53 -0400

    add CSS grid headers dynamically based on mod

--- www/sections/deck_builder/deckBuilderController.js
+++ www/sections/deck_builder/deckBuilderController.js
@@ -2,6 +2,59 @@
 
 "use strict";
 
+const modColumns = {
+    "Cyborg-Chronicles" : [
+        {
+            id : "name",
+            displayName : "Name",
+            width : "1fr",
+            cssProperties : {}
+        },
+        {
+            id : "count",
+            displayName : "Count",
+            width : "1fr",
+            cssProperties : {}
+        },
+        {
+            id : "manaCost",
+            displayName : "Mana Cost",
+            width : "1fr",
+            cssProperties : {}
+        },
+        {
+            id : "attackHealth",
+            displayName : "Attack/Health",
+            width : "1fr"
+        },
+        {
+            id : "scrapValue",
+            displayName : "Scrap Value",
+            width : "1fr"
+        },
+        {
+            id : "sickness",
+            displayName : "Sickness",
+            width : "1fr"
+        },
+        {
+            id : "canAttack",
+            displayName : "Can Attack?",
+            width : "1fr"
+        },
+        {
+            id : "effect",
+            displayName : "Effect",
+            width : "1fr"
+        },
+        {
+            id :"flavor",
+            displayName : "Flavor",
+            width : "1fr"
+        }
+    ]
+};
+
 const deckBuilderController = function() {
     const deckData = {
         cardData : {},
@@ -14,7 +67,7 @@ const deckBuilderController = function() {
     };
     
     const deckBuilderCardsSelected = document.getElementById("deck_builder_cards_selected");
-
+    const deckBuilderCardListTable = document.getElementById("deck_builder_card_list_table");
     
     /**
      * Handles interactions between the browser client and the game server.
@@ -22,7 +75,7 @@ const deckBuilderController = function() {
      */
     const handleWebSocketConnection = function() {
         CardshifterServerAPI.setMessageListener(function(wsMsg) {
-            if (wsMsg.command === "playerconfig") {
+            if (wsMsg.command.toLowerCase() === "playerconfig") {
                 //console.log(JSON.stringify(wsMsg));
                 localStorage.setItem("gameId", wsMsg.gameId);
                 localStorage.setItem("modName", wsMsg.modName);
@@ -37,19 +90,53 @@ const deckBuilderController = function() {
                     deck = null;
                     
                     updateDeckCardSummary();
+                    populateCardListTableHeaders();
                 }
             }
         });
     };
     
+    /**
+     * Update the display of current, min and max cards in deck on the page.
+     * @returns {undefined}
+     */
     const updateDeckCardSummary = function() {
         const cardSummary = `Cards: ${deckData.currentSize} / min: ${deckData.minSize}, max: ${deckData.maxSize}`;
         deckBuilderCardsSelected.innerHTML = cardSummary;
+    };
+    
+    /**
+     * Dynamically create the table headers for displaying cards of a mod, 
+     * based on `modColumns` object
+     * @returns {undefined}
+     */
+    const populateCardListTableHeaders = function() {
+        // Headers
+        const currentMod = localStorage.getItem("modName");
+        const currentModColumns = modColumns[currentMod];
+        const numColumns = currentModColumns.length;
+        let gridTemplateColumns = "";
+        // concatenate widths together in a string for `grid-template-columns` property
+        for (let i = 0; i < numColumns; i++) {
+            gridTemplateColumns += currentModColumns[i].width + " ";
+        }
+        deckBuilderCardListTable.style.gridTemplateColumns = gridTemplateColumns.trim();
+        // make the actual headers
+        for (let i = 0; i < numColumns; i++) {
+            const header = document.createElement("div");
+            header.className = "deckBuilderCardListHeader";
+            header.innerHTML = currentModColumns[i].displayName;
+            for (let prop in currentModColumns[i].cssProperties) {
+                header.style.setProperty(prop, currentModColumns[i].cssProperties[prop]);
+            }
+            deckBuilderCardListTable.appendChild(header);
+        }
     };
 
     /**
      * IIFE to control the deck builder.
      * @type undefined
+     * @returns {undefined}
      */
     const runDeckBuilderController = function() {
         logDebugMessage("deckBuilderController called");
