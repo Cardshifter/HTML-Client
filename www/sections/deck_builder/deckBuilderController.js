086858c09ac99f9a3876d902b0da4bd7ca813f8b
Phrancis <Phrancis@users.noreply.github.com>
2018-04-28 20:34:28 -0400

    add handleWebSocketConnection message listener for "playerconfig" messages

--- www/sections/deck_builder/deckBuilderController.js
+++ www/sections/deck_builder/deckBuilderController.js
@@ -1,6 +1,20 @@
+/* global CardshifterServerAPI */
+
 "use strict";
 
 const deckBuilderController = function() {
+    
+    /**
+     * Handles interactions between the browser client and the game server.
+     * @returns {undefined}
+     */
+    const handleWebSocketConnection = function() {
+        CardshifterServerAPI.setMessageListener(function(wsMsg) {
+            if (wsMsg.command === "playerconfig") {
+                console.log(JSON.stringify(wsMsg));
+            }
+        });
+    };
 
     /**
      * IIFE to control the deck builder.
@@ -8,5 +22,6 @@ const deckBuilderController = function() {
      */
     const runDeckBuilderController = function() {
         logDebugMessage("deckBuilderController called");
+        handleWebSocketConnection();
     }();
 };
