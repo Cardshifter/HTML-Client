292265b7948994c4590c1646f309a94b087ecd0c
Phrancis <Phrancis@users.noreply.github.com>
2018-05-12 01:38:00 -0400

    Hard-coded some pre-loaded decks for use in a game event. A better solution will be implemented in the future

--- src/deck_builder/controller.js
+++ src/deck_builder/controller.js
@@ -13,24 +13,56 @@ function DeckbuilderController(CardshifterServerAPI, $scope, $rootScope, $locati
     $scope.savedDecks = [];
     $scope.doneLoading = false;
     $scope.enteringGame = currentUser.game.id;
+    
+    /**
+     * Hard-coded some pre-loaded decks for use in a game event. A better solution will be implemented in the future.
+     */
+    const preloadedDecks = {"decks":
+        {
+            "Mythos":[
+                {
+                    "name":"Chinese Focus",
+                    "cards":{"4":1,"5":1,"6":1,"7":1,"8":1,"9":1,"10":1,"11":1,"12":1,"18":1,"19":1,"20":1,"21":1,"25":2,"26":2,"27":2,"30":1,"31":2,"32":2,"33":2,"34":2,"35":2}
+                },{
+                    "name":"Greek Classic",
+                    "cards":{"36":1,"37":1,"38":1,"39":1,"40":1,"41":1,"42":1,"43":1,"44":1,"45":1,"46":1,"61":1,"68":1,"69":1,"70":1,"71":1,"72":1,"73":1,"74":1,"75":1,"76":1,"78":1,"79":1,"80":1,"82":1,"85":1,"93":1,"94":1,"97":1,"98":1}
+                },{
+                    "name":"Greek Dark",
+                    "cards":{"9":1,"12":2,"16":2,"17":2,"47":1,"48":1,"49":1,"50":1,"51":1,"52":1,"53":1,"54":1,"55":1,"56":1,"57":1,"58":1,"59":1,"60":1,"62":1,"63":1,"64":1,"65":1,"66":1,"67":1,"95":1,"96":1,"98":1}
+                },{
+                    "name":"Hindu Focus",
+                    "cards":{"3":1,"4":1,"5":1,"6":2,"7":2,"8":2,"9":2,"10":2,"11":1,"12":1,"16":1,"17":1,"18":1,"99":1,"101":1,"103":1,"104":1,"105":1,"106":1,"107":1,"108":1,"109":3,"110":1}
+                }
+            ],"Cyborg-Chronicles":[
+                {
+                    "name":"Balanced",
+                    "cards":{"3":1,"4":1,"5":1,"6":1,"7":1,"8":2,"10":1,"12":2,"14":1,"16":1,"17":1,"19":1,"20":2,"21":1,"22":1,"23":1,"24":1,"25":2,"26":1,"27":1,"28":1,"29":1,"30":1,"31":1,"35":1,"36":1}
+                },{
+                    "name":"Defensive",
+                    "cards":{"3":1,"4":1,"6":2,"7":1,"9":1,"10":2,"12":1,"13":1,"15":2,"16":1,"17":1,"18":1,"19":2,"20":1,"21":2,"22":1,"24":2,"25":1,"26":1,"27":1,"29":1,"32":1,"34":1,"35":1}
+                }
+            ]
+        }
+    };
 
     // This is sort of a repeat of currentDeck, because I didn't know that the client had to send the message back
     var deckConfig = null; // the message received from the server
 
     if(!localStorage.getItem(DECK_STORAGE)) {
-        var mods = {};
-        for (var i in availableGameMods) {
-            mods[availableGameMods[i]] = [];
-        }
-        var json = JSON.stringify({
-            decks: mods
-        });
-        localStorage.setItem(DECK_STORAGE, json);
+//        var mods = {};
+//        for (var i in availableGameMods) {
+//            mods[availableGameMods[i]] = [];
+//        }
+//        var json = JSON.stringify({
+//            decks: mods
+//        });
+//        localStorage.setItem(DECK_STORAGE, json);
+        localStorage.setItem(DECK_STORAGE, JSON.stringify(preloadedDecks));
     }
 
     CardshifterServerAPI.setMessageListener({
         "playerconfig": function(cardInformation) {
-            deckConfig = cardInformation
+            deckConfig = cardInformation;
             var deck = cardInformation.configs.Deck;
 
             for(var card in deck.cardData) {
