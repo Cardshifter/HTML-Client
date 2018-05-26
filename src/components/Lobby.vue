79f0210b10491073fe0b03b16c3473ebbe6162dc
Phrancis <Phrancis@users.noreply.github.com>
2018-05-26 00:02:49 -0400

    apply same indentation to whole file

--- src/components/Lobby.vue
+++ src/components/Lobby.vue
@@ -1,14 +1,14 @@
 <template>
-  <table id="lobby" class="lobby">
-      <tr id="lobby-headers" class="lobby-headers">
+    <table id="lobby" class="lobby">
+        <tr id="lobby-headers" class="lobby-headers">
           <td id="lobby-title" class="lobby-title">Lobby</td>
           <td id="lobby-deck-builder" class="lobby-deck-builder" width="20%">
               <button @click="openDeckBuilder()" class="btn btn-navbar csh-button deck-builder-btn">
                   Deck Builder
               </button>
           </td>
-      </tr>
-      <tr id="lobby-invite-request"class="lobby-invite-request" v-if="gotInvite">
+        </tr>
+        <tr id="lobby-invite-request"class="lobby-invite-request" v-if="gotInvite">
           <td colspan="2">
               <div id="lobby-accept-invite" class="lobby-accept-invite">
                   Game invite from {{invite.name}} to play {{invite.type}}!<br/>
@@ -19,12 +19,12 @@
                   </audio>
               </div>
           </td>
-      </tr>
-      <tr id="lobby-list-headers" class="lobby-list-headers">
+        </tr>
+        <tr id="lobby-list-headers" class="lobby-list-headers">
           <th id="lobby-message-list-header" class="lobby-message-list-header">Messages</th>
           <th id="lobby-users-list-header" class="lobby-users-list-header">Users Online</th>
-      </tr>
-      <tr id="lobby-lists" class="lobby-lists">
+        </tr>
+        <tr id="lobby-lists" class="lobby-lists">
           <td id="lobby-message-list" class="lobby-message-list">
               <ul id="lobby-chat-messages" class="lobby-chat-messages">
                   <li v-for="message in chatMessages" :class="{'user-chat-message lobby-chat-message': message.from === currentUser.username}" id="lobby-chat-message">
@@ -43,8 +43,8 @@
                   </li>
               </ul>
           </td>
-      </tr>
-      <tr>
+        </tr>
+        <tr>
           <td id="lobby-message" class="lobby-message">
               <textarea v-model="user_chat_message" @keyup.enter="sendMessage($event)"
                         id="lobby-chat-text-area" class="lobby-chat-text-area" rows="1" cols="75" wrap="off"
@@ -54,8 +54,8 @@
           <td id="lobby-invite" class="lobby-invite">
               <input @click="startGame()" type="button" value="Invite to game!" class="btn btn-success lobby-game-invite-btn"/>
           </td>
-      </tr>
-      <tr id="lobby-mods" class="lobby-mods">
+        </tr>
+        <tr id="lobby-mods" class="lobby-mods">
           <td colspan="2" id="lobby-mod-selection" class="lobby-mod-selection">
               <form class="form-inline" role="form">
                   <div class="form-group">
@@ -70,8 +70,8 @@
                   </div>
               </form>
           </td>
-      </tr>
-  </table>
+        </tr>
+    </table>
 </template>
 
 <script>
@@ -99,203 +99,210 @@ function displayError(message) {
 }
 
 export default {
-  name: "Lobby",
-  props: ["currentUser"],
-  data() {
-    return {
-      users: [],
-      chatMessages: [],
-      user_chat_message: "",
-      sending: false,
-      mods: [],
-      selected_mod: null,
-      selected_opponent: null,
-      invite: {
-        id: null,
-        name: null,
-        type: null
-      },
-      gotInvite: false
-    }
-  },
-  methods: {
-    /**
-    * This function is called when the user hits the "Send" button
-    * write text to the chat message text box.
-    *
-    * Upon click the send button or hitting the enter key, this function
-    * will send a new ChatMessage to the server. Then, the clear the
-    * chat message input box and disable use of the send button
-    * for the time specified in MESSAGE_DELAY
-    */
-    sendMessage(e) {
-      if (this.sending) {
-          return;
-      }
-      if (this.user_chat_message.trim().length === 0) {
-          return;
-      }
-
-      this.sending = true;
-      var chatMessage = new CardshifterServerAPI.messageTypes.ChatMessage(this.user_chat_message);
-      CardshifterServerAPI.sendMessage(chatMessage);
-
-      this.user_chat_message = ""; // clear the input box
-      setTimeout(() => { this.sending = false }, MESSAGE_DELAY);
-    },
-
-    /**
-    * This function is called when the user has chosen a mod,
-    * selected an opponent, and hit the "invite" button.
-    *
-    * This function sends a StartGameRequest to the server.
-    */
-    startGame() {
-        if (this.selected_mod && this.selected_opponent) {
-            var startGame = new CardshifterServerAPI.messageTypes.StartGameRequest(this.selected_opponent,
-               this.selected_mod);
-            CardshifterServerAPI.sendMessage(startGame);
-        } else {
-            // Error if user has not chosen a mod or opponent
-            ErrorCreator.create("Select both a game type and an opponent user before you can start a game.");
+    name: "Lobby",
+    props: ["currentUser"],
+    data() {
+        return {
+            users: [],
+            chatMessages: [],
+            user_chat_message: "",
+            sending: false,
+            mods: [],
+            selected_mod: null,
+            selected_opponent: null,
+            invite: {
+                id: null,
+                name: null,
+                type: null
+            },
+            gotInvite: false
         }
     },
-
-    /**
-    * This function is called when either the "accept" or "decline"
-    * button of the invite pop-up has been clicked.
-    *
-    * This function sends an InviteResponse message to the server and
-    * and passes in the accept argument to the constructor. If the
-    * user hit "accept", then the accept argument will be true. If
-    * the user hit "decline", then the accept argument will be false.
-    *
-    * @param accept:boolean -- true for "accept"
-                            -- false for "decline"
-    */
-    acceptInvite(accept) {
-      if (accept) {
-        this.selected_mod = this.invite.type;
-      }
-      var accept = new CardshifterServerAPI.messageTypes.InviteResponse(this.invite.id, accept);
-      CardshifterServerAPI.sendMessage(accept);
-      this.gotInvite = false;
-    },
-
-    /**
-    * This function is called once the user has selected a mod
-    * and has clicked the "Deck Builder" button near the top of the
-    * screen. If the user has not yet selected a mod, then this
-    * function does nothing.
-    *
-    * Once this is run, a ServerQueryMessage is sent to the server
-    * to retrieve all the cards. The reason why this has to be sent
-    * manually is because the server does not know when the user
-    * is entering the deck builder, so it does not know to send
-    * the card information automatically, as opposed to if the user
-    * were entering a new game.
-    */
-    openDeckBuilder() {
-        if (this.selected_mod) {
-            this.currentUser.game.mod = this.selected_mod;
-
-            var getCards = new CardshifterServerAPI.messageTypes.ServerQueryMessage("DECK_BUILDER", this.currentUser.game.mod);
-            CardshifterServerAPI.sendMessage(getCards);
-            this.$router.push({ name: 'DeckBuilder', params: {
-              currentUser: currentUser
-            }});
-        } else {
-            ErrorCreator.create("Select a game type before you can open the deck builder.");
-        }
-    },
-
-    // The command map functions:
-    /**
-    * Based on the content of message, will add or remove
-    * a user from the user list.
-    */
-    updateUserList(message) {
-        if (message.status === "OFFLINE") {
-            for (var i = 0, length = this.users.length; i < length; i++) {
-                if (this.users[i].userId === message.userId) {
-                    this.users.splice(i, 1); // remove that user from the array
-                    return;
-                }
+    methods: {
+        /**
+         * This function is called when the user hits the "Send" button
+         * write text to the chat message text box.
+         *
+         * Upon click the send button or hitting the enter key, this function
+         * will send a new ChatMessage to the server. Then, the clear the
+         * chat message input box and disable use of the send button
+         * for the time specified in MESSAGE_DELAY
+         */
+        sendMessage(e) {
+            if (this.sending) {
+                return;
             }
-        } else {
-          this.users.push(message);
+            if (this.user_chat_message.trim().length === 0) {
+                return;
+            }
+            this.sending = true;
+            var chatMessage = new CardshifterServerAPI.messageTypes.ChatMessage(this.user_chat_message);
+            CardshifterServerAPI.sendMessage(chatMessage);
+
+            this.user_chat_message = ""; // clear the input box
+            setTimeout(() => {
+                this.sending = false
+            }, MESSAGE_DELAY);
+        },
+
+        /**
+         * This function is called when the user has chosen a mod,
+         * selected an opponent, and hit the "invite" button.
+         *
+         * This function sends a StartGameRequest to the server.
+         */
+        startGame() {
+            if (this.selected_mod && this.selected_opponent) {
+                var startGame = new CardshifterServerAPI.messageTypes.StartGameRequest(this.selected_opponent, this.selected_mod);
+                CardshifterServerAPI.sendMessage(startGame);
+            } else {
+                // Error if user has not chosen a mod or opponent
+                ErrorCreator.create("Select both a game type and an opponent user before you can start a game.");
+            }
+        },
+
+        /**
+        * This function is called when either the "accept" or "decline"
+        * button of the invite pop-up has been clicked.
+        *
+        * This function sends an InviteResponse message to the server and
+        * and passes in the accept argument to the constructor. If the
+        * user hit "accept", then the accept argument will be true. If
+        * the user hit "decline", then the accept argument will be false.
+        *
+        * @param accept:boolean -- true for "accept"
+                                -- false for "decline"
+        */
+        acceptInvite(accept) {
+            if (accept) {
+                this.selected_mod = this.invite.type;
+            }
+            var accept = new CardshifterServerAPI.messageTypes.InviteResponse(this.invite.id, accept);
+            CardshifterServerAPI.sendMessage(accept);
+            this.gotInvite = false;
+        },
+
+        /**
+         * This function is called once the user has selected a mod
+         * and has clicked the "Deck Builder" button near the top of the
+         * screen. If the user has not yet selected a mod, then this
+         * function does nothing.
+         *
+         * Once this is run, a ServerQueryMessage is sent to the server
+         * to retrieve all the cards. The reason why this has to be sent
+         * manually is because the server does not know when the user
+         * is entering the deck builder, so it does not know to send
+         * the card information automatically, as opposed to if the user
+         * were entering a new game.
+         */
+        openDeckBuilder() {
+            if (this.selected_mod) {
+                this.currentUser.game.mod = this.selected_mod;
+
+                var getCards = new CardshifterServerAPI.messageTypes.ServerQueryMessage("DECK_BUILDER", this.currentUser.game.mod);
+                CardshifterServerAPI.sendMessage(getCards);
+                this.$router.push({
+                    name: 'DeckBuilder',
+                    params: {
+                        currentUser: currentUser
+                    }
+                });
+            } else {
+                ErrorCreator.create("Select a game type before you can open the deck builder.");
+            }
+        },
+
+        // The command map functions:
+        /**
+         * Based on the content of message, will add or remove
+         * a user from the user list.
+         */
+        updateUserList(message) {
+            if (message.status === "OFFLINE") {
+                for (var i = 0, length = this.users.length; i < length; i++) {
+                    if (this.users[i].userId === message.userId) {
+                        this.users.splice(i, 1); // remove that user from the array
+                        return;
+                    }
+                }
+            } else {
+                this.users.push(message);
+            }
+        },
+        /**
+         * Adds a chat message to the message feed. If the message
+         * feed is at the maximum limit of messages, deletes the oldest
+         * message.
+         */
+        addChatMessage(message) {
+            if (this.chatMessages.length === CHAT_FEED_LIMIT) {
+                // remove the oldest chat message
+                this.chatMessages.shift();
+            }
+
+            var now = new Date();
+
+            var YMD = [formatTimeNumber(now.getFullYear()), formatTimeNumber(now.getMonth() + 1), formatTimeNumber(now.getDate())].join('-');
+            var HMS = [formatTimeNumber(now.getHours()), formatTimeNumber(now.getMinutes()), formatTimeNumber(now.getSeconds())].join(':');
+            message.timestamp = YMD + " " + HMS;
+
+            this.chatMessages.push(message);
+        },
+        /**
+         * Shows buttons and a message to this client for accepting
+         * or declining a game request.
+         */
+        displayInvite(message) {
+            this.invite.id = message.id;
+            this.invite.name = message.name;
+            this.invite.type = message.gameType;
+            this.gotInvite = true;
+            this.$refs.pingAudio.play();
+        },
+        /**
+         * Shows to the user a list of all available mods.
+         */
+        displayMods(message) {
+            this.mods = message.mods;
+        },
+        /**
+         * Stores the game ID in currentUser for other controllers
+         * to use and navigates to the deck-builder page for the
+         * user to select a deck.
+         */
+        enterNewGame(message) {
+            this.currentUser.game.id = message.gameId;
+            this.currentUser.game.mod = this.selected_mod;
+            this.currentUser.game.playerIndex = message.playerIndex;
+
+            this.$router.push({
+                name: 'DeckBuilder',
+                params: {
+                    currentUser: this.currentUser
+                }
+            });
         }
     },
-    /**
-    * Adds a chat message to the message feed. If the message
-    * feed is at the maximum limit of messages, deletes the oldest
-    * message.
-    */
-    addChatMessage(message) {
-        if (this.chatMessages.length === CHAT_FEED_LIMIT) {
-            // remove the oldest chat message
-            this.chatMessages.shift();
-        }
+    created() {
+        CardshifterServerAPI.$on("type:userstatus", this.updateUserList);
+        CardshifterServerAPI.$on("type:chat", this.addChatMessage);
+        CardshifterServerAPI.$on("type:inviteRequest", this.displayInvite);
+        CardshifterServerAPI.$on("type:availableMods", this.displayMods);
+        CardshifterServerAPI.$on("type:newgame", this.enterNewGame);
+        CardshifterServerAPI.$on("type:error", this.displayError);
 
-        var now = new Date();
-
-        var YMD = [formatTimeNumber(now.getFullYear()), formatTimeNumber(now.getMonth() + 1), formatTimeNumber(now.getDate())].join('-');
-        var HMS = [formatTimeNumber(now.getHours()), formatTimeNumber(now.getMinutes()), formatTimeNumber(now.getSeconds())].join(':');
-        message.timestamp = YMD + " " + HMS;
-
-        this.chatMessages.push(message);
+        CardshifterServerAPI.sendMessage(new CardshifterServerAPI.messageTypes.ServerQueryMessage("USERS", ""));
+        CardshifterServerAPI.sendMessage(new CardshifterServerAPI.messageTypes.ServerQueryMessage("MODS", ""));
     },
-    /**
-    * Shows buttons and a message to this client for accepting
-    * or declining a game request.
-    */
-    displayInvite(message) {
-        this.invite.id = message.id;
-        this.invite.name = message.name;
-        this.invite.type = message.gameType;
-        this.gotInvite = true;
-        this.$refs.pingAudio.play();
-    },
-    /**
-    * Shows to the user a list of all available mods.
-    */
-    displayMods(message) {
-      this.mods = message.mods;
-    },
-    /**
-    * Stores the game ID in currentUser for other controllers
-    * to use and navigates to the deck-builder page for the
-    * user to select a deck.
-    */
-    enterNewGame(message) {
-      this.currentUser.game.id = message.gameId;
-      this.currentUser.game.mod = this.selected_mod;
-      this.currentUser.game.playerIndex = message.playerIndex;
-
-      this.$router.push({ name: 'DeckBuilder', params: { currentUser: this.currentUser }});
+    computed: {},
+    beforeDestroy() {
+        CardshifterServerAPI.$off("type:userstatus", this.updateUserList);
+        CardshifterServerAPI.$off("type:chat", this.addChatMessage);
+        CardshifterServerAPI.$off("type:inviteRequest", this.displayInvite);
+        CardshifterServerAPI.$off("type:availableMods", this.displayMods);
+        CardshifterServerAPI.$off("type:newgame", this.enterNewGame);
+        CardshifterServerAPI.$off("type:error", this.displayError);
     }
-  },
-  created() {
-    CardshifterServerAPI.$on("type:userstatus", this.updateUserList);
-    CardshifterServerAPI.$on("type:chat", this.addChatMessage);
-    CardshifterServerAPI.$on("type:inviteRequest", this.displayInvite);
-    CardshifterServerAPI.$on("type:availableMods", this.displayMods);
-    CardshifterServerAPI.$on("type:newgame", this.enterNewGame);
-    CardshifterServerAPI.$on("type:error", this.displayError);
-
-    CardshifterServerAPI.sendMessage(new CardshifterServerAPI.messageTypes.ServerQueryMessage("USERS", ""));
-    CardshifterServerAPI.sendMessage(new CardshifterServerAPI.messageTypes.ServerQueryMessage("MODS", ""));
-  },
-  computed: {
-  },
-  beforeDestroy() {
-    CardshifterServerAPI.$off("type:userstatus", this.updateUserList);
-    CardshifterServerAPI.$off("type:chat", this.addChatMessage);
-    CardshifterServerAPI.$off("type:inviteRequest", this.displayInvite);
-    CardshifterServerAPI.$off("type:availableMods", this.displayMods);
-    CardshifterServerAPI.$off("type:newgame", this.enterNewGame);
-    CardshifterServerAPI.$off("type:error", this.displayError);
-  }
 };
 </script>
 
