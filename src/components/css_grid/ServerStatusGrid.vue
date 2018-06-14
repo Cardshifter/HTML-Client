59b6e69d18ea8f7c5263b1119552ae3acb32ed89
Phrancis <Phrancis@users.noreply.github.com>
2018-06-13 23:25:25 -0400

    Make a basic template for server status grid for testing

---
+++ src/components/css_grid/ServerStatusGrid.vue
@@ -0,0 +1,33 @@
+<template>
+    <css-grid
+        :columns="['auto', 'auto', 'auto', 'auto', 'auto', 'auto', 'auto']"
+        :auto-rows="'auto'">
+
+        <!-- Headers -->
+        <css-grid-item
+            class="server-status-header"
+            v-for="(headerName, index) in headerNames"
+            :key="index">
+            <div>{{headerName}}</div>
+        </css-grid-item>
+
+    </css-grid>
+</template>
+<script>
+export default {
+    name: "ServerStatusGrid",
+    data() {
+        return {
+            headerNames: [
+                "Server",
+                "Online",
+                "Users",
+                "Mods",
+                "Games",
+                "AIs",
+                "Latency"
+            ]
+        }
+    },
+}
+</script>
